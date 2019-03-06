f2d37e1f802126506d9b3201b3fb23c65e61566d
Rogbehrt <42278986+Rogbehrt@users.noreply.github.com>
2019-03-06 04:21:52 +0000

    test

---
+++ gracias.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+import time
+import Adafruit_CharLCD as LCD
+
+# LCD settings
+lcd_rs        = 12
+lcd_en        = 06
+lcd_d4        = 20
+lcd_d5        = 26
+lcd_d6        = 16
+lcd_d7        = 19
+lcd_backlight = 4
+lcd_columns   = 16
+lcd_rows      = 2
+lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
+
+def s(t=0.33):
+    return time.sleep(t)
+
+lcd.blink(True)
+lcd.message('Hola Papa')
+s(2)
+lcd.clear()
+lcd.blink(True)
+lcd.message('La pantalla es\nazul')
+s(2)
+lcd.clear()
+lcd.blink(True)
+lcd.message('Y las letras son\nblancas')
+s(2)
+lcd.clear()
