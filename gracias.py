75ad41aa00d9cb78c87a74c52b8f3f968e29c512
Rogbehrt <42278986+Rogbehrt@users.noreply.github.com>
2019-03-06 04:41:07 +0000

    a

--- gracias.py
+++ gracias.py
@@ -17,19 +17,16 @@ lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_c
 def s(t=0.33):
     return time.sleep(t)
 
-lcd.blink(True)
-lcd.message('Hola Papa')
-s(2)
-lcd.clear()
-lcd.blink(True)
-lcd.message('La pantalla es\nazul')
-s(2)
-lcd.clear()
-lcd.blink(True)
-lcd.message('Y las letras son\nblancas')
-s(2)
-lcd.clear()
-lcd.blink(True)
-lcd.message('Queria darte las\ngracias por')
-s(2)
-lcd.clear()
+def mensaje(lista):
+    for parte in lista:
+        lcd.clear()
+        lcd.blick(True)
+        lcd.message(parte)
+        s(2)
+
+# Partes del mensaje
+p1 = 'Hola Papa'
+p2 = 'La pantalla es\nazul'
+p3 = 'Y las letras son\nblancas'
+mensaje = [p1,p2,p3]
+mensaje(mensaje)
